dde14ebacd79c3a0d4eb77e11bdc45cbf2e4bfc8
glebkirill99 <glebkirill99@gmail.com>
2022-10-31 20:48:26 +0300

    create files for new activity

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemDiffCallBack.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemDiffCallBack.kt
@@ -6,6 +6,5 @@ import com.example.shoppinglist.domain.ShopItem
 class ShopItemDiffCallBack : DiffUtil.ItemCallback<ShopItem>() {
 
     override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem) = oldItem.id == newItem.id
-
     override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem) = oldItem == newItem
 }
